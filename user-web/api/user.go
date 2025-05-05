a8b61bcabdd6b3a0e3b7ba6878033ac7f7873ef9
Walker <zw37520@gmail.com>
2025-05-05 21:26:40 +0700

    用户信息更新添加username

--- user-web/api/user.go
+++ user-web/api/user.go
@@ -2,15 +2,17 @@ package api
 
 import (
 	"fmt"
+	"joyshop_api/user-web/global"
+	"joyshop_api/user-web/proto"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
-	"joyshop_api/user-web/global"
-	"joyshop_api/user-web/proto"
-	"net/http"
 )
 
 func HandleGrpcErrorToHttp(err error, c *gin.Context) {
@@ -60,7 +62,32 @@ func GetUserList(ctx *gin.Context) {
 	// 生成grpc客户端
 	userSrvClient := proto.NewUserClient(userConn)
 	// 调用grpc服务
-	userResp, err := userSrvClient.GetUserList(ctx, &proto.PageInfo{})
+	page := ctx.DefaultQuery("page", "1")
+	pageSize := ctx.DefaultQuery("pageSize", "10")
+	pageInt, err := strconv.Atoi(page)
+
+	if err != nil {
+		zap.S().Errorw("[GetUserList] page 转换失败", "msg", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "参数错误",
+		})
+		return
+	}
+	paseSizeInt, err := strconv.Atoi(pageSize)
+	if err != nil {
+		zap.S().Errorw("[GetUserList] pageSize 转换失败", "msg", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "参数错误",
+		})
+		return
+	}
+
+	userResp, err := userSrvClient.GetUserList(ctx, &proto.PageInfo{
+		Page:     uint32(pageInt),
+		PageSize: uint32(paseSizeInt),
+	})
 	if err != nil {
 		zap.S().Errorw("[GetUserList] 调用 【用户服务失败】", "msg", err.Error())
 		HandleGrpcErrorToHttp(err, ctx)
@@ -72,6 +99,7 @@ func GetUserList(ctx *gin.Context) {
 		data := make(map[string]interface{})
 		data["id"] = user.Id
 		data["nickName"] = user.Nickname
+		data["username"] = user.Username
 		data["mobile"] = user.Mobile
 		data["email"] = user.Email
 		data["avatar"] = user.Avatar
@@ -83,9 +111,10 @@ func GetUserList(ctx *gin.Context) {
 	}
 	// 返回数据
 	ctx.JSON(http.StatusOK, gin.H{
-		"code": 0,
-		"msg":  "success",
-		"data": result,
+		"code":  0,
+		"msg":   "success",
+		"total": userResp.Total,
+		"data":  result,
 	})
 }
 
@@ -96,5 +125,84 @@ func UpdateUser(context *gin.Context) {
 
 }
 func DeleteUser(context *gin.Context) {
+
+}
+
+func PassWordLogin(ctx *gin.Context) {
+	// 获取请求参数
+	var request struct {
+		Mobile   string `json:"mobile"`
+		Password string `json:"password"`
+	}
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "参数错误",
+		})
+		return
+	}
+
+	// 连接用户服务
+	userConn, err := grpc.NewClient(fmt.Sprintf("%s:%d", global.ServerConfig.UserSrvInfo.Host, global.ServerConfig.UserSrvInfo.Port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		zap.S().Errorw("[PassWordLogin] 连接 【用户服务失败】", "msg", err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "服务器内部错误",
+		})
+		return
+	}
+	defer userConn.Close()
+
+	// 生成grpc客户端
+	userSrvClient := proto.NewUserClient(userConn)
+
+	// 1. 先通过手机号获取用户信息
+	userInfo, err := userSrvClient.GetUserByMobile(ctx, &proto.MobileRequest{
+		Mobile: request.Mobile,
+	})
+	if err != nil {
+		zap.S().Errorw("[PassWordLogin] 获取用户信息失败", "msg", err.Error())
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	// 2. 验证密码
+	checkResp, err := userSrvClient.CheckPassword(ctx, &proto.PasswordCheckInof{
+		Password:        request.Password,
+		EncryptPassword: userInfo.Password,
+	})
+	if err != nil {
+		zap.S().Errorw("[PassWordLogin] 验证密码失败", "msg", err.Error())
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	if !checkResp.Success {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"code": 401,
+			"msg":  "密码错误",
+		})
+		return
+	}
+
+	// 3. 返回用户信息
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  "登录成功",
+		"data": gin.H{
+			"id":       userInfo.Id,
+			"nickName": userInfo.Nickname,
+			"mobile":   userInfo.Mobile,
+			"email":    userInfo.Email,
+			"avatar":   userInfo.Avatar,
+			"gender":   userInfo.Gender,
+			"birthday": userInfo.Birthday,
+			"role":     userInfo.Role,
+		},
+	})
+}
+
+func SendSms(context *gin.Context) {
 
 }
--- user-web/router/user.go
+++ user-web/router/user.go
@@ -1,8 +1,9 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"joyshop_api/user-web/api"
+
+	"github.com/gin-gonic/gin"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
@@ -12,6 +13,6 @@ func InitUserRouter(Router *gin.RouterGroup) {
 		UserRouter.POST("create", api.CreateUser)
 		UserRouter.PUT("update", api.UpdateUser)
 		UserRouter.DELETE("delete", api.DeleteUser)
+		UserRouter.POST("password_login", api.PassWordLogin)
 	}
-
 }
